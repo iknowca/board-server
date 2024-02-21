56a6b995f3adbbb262251946f3ae39bada1db21c
iknowca <iknowca@gmail.com>
2024-02-22 04:08:55 +0900

    init: board server 프로젝트 생성 [IBS-1]

---
+++ src/main/java/com/example/iknowboardserver/IknowBoardServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.iknowboardserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IknowBoardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IknowBoardServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/iknowboardserver/IknowBoardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iknowboardserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IknowBoardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
